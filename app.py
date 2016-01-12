f633ca2ca0b824190438e8960adcd02cf090e506
David Kubeša <d.kubesa@gmail.com>
2016-01-12 11:03:05 +0100

    Added error handling

--- app.py
+++ app.py
@@ -11,8 +11,9 @@ import os
 import unirest
 import json
 import urllib2
+import math
 from geopy.distance import vincenty
-from flask import Flask, render_template, request, redirect, url_for,make_response,jsonify
+from flask import Flask, render_template, request, redirect, url_for,make_response,jsonify,abort as flask_abort
 from signal import signal, SIGPIPE, SIG_DFL
 signal(SIGPIPE,SIG_DFL)
 app = Flask(__name__)
@@ -85,10 +86,17 @@ def process_fact(fact,data,maxdistance,bestknownlife,latstart,lngstart,datacount
 
 @app.route('/api/')
 def api():
-    max_distance = float(request.args.get('max_distance'))
-    latstart = float(request.args.get('latitude'))
-    lngstart = float(request.args.get('longitude'))
-    print(max_distance,latstart,lngstart)
+    try:
+        max_distance = float(request.args.get('max_distance'))
+        latstart = float(request.args.get('latitude'))
+        lngstart = float(request.args.get('longitude'))
+        print(max_distance,latstart,lngstart)
+    except ValueError:
+        return jsonify({'code':400,'text': 'Bad request, should be the Maximal distance,'
+                                                   'Latitude component of location,Longitude component of location. All as numbers'})
+    if math.isnan(max_distance) | math.isnan(latstart) | math.isnan(lngstart):
+        return jsonify({'code':400,'text': 'Bad request, should be the Maximal distance,'
+                                                   'Latitude component of location,Longitude component of location. All as numbers'})
 
     data = json.load(urllib2.urlopen('http://apps.who.int/gho/athena/api/GHO/WHOSIS_000001.json'))
     dataset = data['fact']
@@ -100,14 +108,18 @@ def api():
             "Accept": "application/json"
         }
     )
-
+    bestknowncountry = None
     for fact in dataset:
         abc = process_fact(fact,data,float(max_distance),besknownage,float(latstart),float(lngstart),datacountries)
         if abc is not None:
             bestknowncountry = abc
             besknownage = abc[1]
-    print (bestknowncountry[0],bestknowncountry[2],bestknowncountry[3])
-    return jsonify({'state': bestknowncountry[0],'latitude': bestknowncountry[2],'longitude': bestknowncountry[3]})
+    if bestknowncountry is not None:
+        print (bestknowncountry[0],bestknowncountry[2],bestknowncountry[3])
+        return jsonify({'state': bestknowncountry[0],'latitude': bestknowncountry[2],'longitude': bestknowncountry[3]})
+    else:
+       return jsonify({'code':400,'text': 'No country in selected distance.'
+                                                   })
 
 ###
 # The functions below should be applicable to all Flask apps.
@@ -130,7 +142,10 @@ def add_header(response):
     response.headers['Cache-Control'] = 'public, max-age=600'
     return response
 
-
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify({'code':400,'text': 'Bad request, should be the Maximal distance,'
+                                                   'Latitude component of location,Longitude component of location. All as numbers'}), 400)
 @app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
